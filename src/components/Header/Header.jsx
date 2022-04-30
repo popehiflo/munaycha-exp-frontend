0e093a8da1e1a08c71167d180759a3a31748eed6
popehiflo <poolpetterhijuel@hotmail.com>
2022-04-30 17:32:12 -0500

    feat: Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,45 @@
-import React, { useRef } from 'react';
+import { useRef, useState } from 'react';
 import './Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-import logoIconText from '../../assets/imgs/logo-icon-text.svg';
+import { faBars, faShoppingCart, faTimes } from '@fortawesome/free-solid-svg-icons';
+import logoIconGoldText from '../../assets/imgs/logo-icon-gold-text.svg';
 
 const Header = () => {
   const navMenu = useRef(null);
-  const showMenu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleClickOpenMenu = () => {
     navMenu.current.classList.toggle('active');
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
     <header className="header">
       <a href="/" className="logo">
-        <img src={logoIconText} alt="logo" />
+        <img src={logoIconGoldText} alt="logo" />
       </a>
-      <nav className="navbar" ref={navMenu}>
-        {/* <div className="navbar-menu" > */}
-        <a href="/" className="">Nav Item 1</a>
-        <a href="/" className="">Nav Item 2</a>
-        <a href="/" className="">Nav Item 3</a>
-        <a href="/" className="">Nav Item 4</a>
-        <a href="/" className="">Login</a>
-        {/* </div> */}
-      </nav>
-      <button className="menu-button btn-icon" type="button" onClick={showMenu} aria-label="Open Shopping Cart">
-        <FontAwesomeIcon icon={faShoppingCart} />
-      </button>
-      <button id="menu-btn" className="menu-button-ext btn-icon" type="button" onClick={showMenu} aria-label="Open Menu">
-        <FontAwesomeIcon icon={faBars} />
-      </button>
+      <div className="nav-wrapper">
+        <nav className="navbar" ref={navMenu}>
+          {/* <div className="navbar-menu" > */}
+          <a href="/" className="">Nav Item 1</a>
+          <a href="/" className="">Nav Item 2</a>
+          <a href="/" className="">Nav Item 3</a>
+          <a href="/" className="">Nav Item 4</a>
+          <a href="/" className="">Login</a>
+          {/* </div> */}
+        </nav>
+        <button className="menu-button btn-icon" type="button" aria-label="Open Shopping Cart">
+          <FontAwesomeIcon icon={faShoppingCart} />
+        </button>
+        <button
+          className="menu-button-ext btn-icon"
+          type="button"
+          onClick={handleClickOpenMenu}
+          id="menu-btn"
+          aria-label={isMenuOpen ? 'Close Menu' : 'Open Menu'}
+        >
+          <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} />
+        </button>
+      </div>
     </header>
   );
 };
