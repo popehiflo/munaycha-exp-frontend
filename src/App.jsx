268f2530895b569b9dc6046477eb76e65f938706
popehiflo <poolpetterhijuel@hotmail.com>
2022-05-01 00:05:32 -0500

    feat: test AOS

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,60 @@
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import logo from './assets/imgs/logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 
-const App = () => (
-  <>
-    <Header />
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link btn-primary"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  </>
-);
+const App = () => {
+  AOS.init({
+    duration: 800,
+    offset: 150,
+  });
+
+  return (
+    <>
+      <Header />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit
+            {' '}
+            <code>src/App.js</code>
+            {' '}
+            and save to reload.
+          </p>
+          <a
+            className="App-link btn-primary"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit
+            {' '}
+            <code>src/App.js</code>
+            {' '}
+            and save to reload.
+          </p>
+          <a
+            className="App-link btn-primary"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
+  );
+};
 
 export default App;
