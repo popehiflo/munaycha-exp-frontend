c65cf0ec699ed12663a3cfa4c9390abefd61af9d
popehiflo <poolpetterhijuel@hotmail.com>
2022-05-07 09:09:44 -0500

    feat: About component, created

---
+++ src/components/About/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './About.css';
+
+const About = () => (
+  <section className="about" id="about">
+    About
+  </section>
+);
+
+export default About;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import About from '../../components/About/About';
 import Hero from '../../components/Hero/Hero';
 import Tours from '../Tours/Tours';
 
 const Home = () => (
   <>
     <Hero />
+    <About />
     <Tours />
   </>
 );
