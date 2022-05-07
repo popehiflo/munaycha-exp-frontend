745fe59d2b9ff22ddc7d2a72762dc5f6233e325e
popehiflo <poolpetterhijuel@hotmail.com>
2022-05-06 20:56:33 -0500

    feat: list tours and fix CSS

--- src/pages/Tours/Tours.jsx
+++ src/pages/Tours/Tours.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import './Tours.css';
-import { dataPopularTours } from '../../fakeData';
+import { getToursByPopularity } from '../../fakeData';
 import TourCard from '../../components/TourCard/TourCard';
 
-const Tours = () => (
-  <section className="tours">
-    <div className="heading">
-      <span>Tours diseñados para conocer, compartir y disfrutar del Perú.</span>
-      <h1>Nuestros Tours</h1>
-    </div>
-    <div className="box-container">
-      {dataPopularTours.map((tour) => (
-        <TourCard key={tour.id} tour={tour} />
-      ))}
-    </div>
-  </section>
-);
+const Tours = () => {
+  const tours = getToursByPopularity();
+  return (
+    <section className="tours">
+      <div className="heading">
+        <span>Tours diseñados para conocer, compartir y disfrutar del Perú.</span>
+        <h1>Nuestros Tours</h1>
+      </div>
+      <div className="box-container">
+        {tours.map((tour) => (
+          <TourCard key={tour.id} tour={tour} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default Tours;
